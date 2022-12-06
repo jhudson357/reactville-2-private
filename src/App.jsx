d46b565943df03d636eb6a80378653dc19a68e56
James Hudson <jmh357@comcast.net>
2022-12-06 15:06:25 -0500

    add route to burger shop

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
 import './styles/App.css'
 
 // Components
 import Landing from './Components/Landing/Landing'
+import BurgerShop from './Components/BurgerShop/BurgerShop'
+import Nav from './Components/Nav/Nav'
 
 const App = () => {
   return (
-    <main>
-      <Landing />
-    </main>
+    <>
+      <Nav />
+      <main>
+        <Routes>
+          <Route path='/' element={<Landing />} />
+          <Route path='/burgers' element={<BurgerShop />} />
+        </Routes>
+        <Landing />
+      </main>
+    </>
   )
 }
 
--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -1,10 +1,15 @@
+import { NavLink } from 'react-router-dom'
+
 // Components & Assets
 import Logo from '../../assets/react-logo.png'
 
 const Nav = () => {
   return (
     <nav className="navigation-bar">
-
+      <NavLink to='/' id='logo'>
+        <img src={Logo} alt="logo" />
+      </NavLink>
+      <NavLink to='/burgers'>BURGER SHOP</NavLink>
     </nav>
   )
 }
